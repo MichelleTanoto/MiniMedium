5ed5fc5b09f1bcbaf50b469ba121f7b9e8ee337e
Michelle <michelletanotoo@gmail.com>
2019-11-18 16:29:36 +1100

    some bug fixes in calling .env file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
-import middleware from './utils/middleware'; // another way to import other file functions
-
-const config = require('./utils/config') 
+require('dotenv').config({path:'./.env'}) // why
+const config = require('./utils/config')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const postRouter = require('./router/routes'); // to use the express routes
-// const middleware = require('./utils/middleware');
+const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
 console.log('connecting to', config.URI)
@@ -27,7 +26,7 @@ app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 app.listen(config.PORT || 3000, () => {
-    console.log(`Server is running on port: ${config.port}`);
+    console.log(`Server is running on port: ${config.PORT}`);
 });
 
 module.exports = app
---
+++ src/utils/config.js
@@ -0,0 +1,8 @@
+// require('dotenv').config({path:'../.env'})
+let PORT = process.env.PORT
+let URI = process.env.URI
+
+module.exports = {
+  URI,
+  PORT
+}
