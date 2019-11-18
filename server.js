4f6298f05654dbd97d4c2c17b69899f2fa11e701
Michelle <michelletanotoo@gmail.com>
2019-11-18 15:42:49 +1100

    add backend and simple frontend functionality

---
+++ server.js
@@ -0,0 +1,33 @@
+import middleware from './utils/middleware'; // another way to import other file functions
+
+const config = require('./utils/config') 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const postRouter = require('./router/routes'); // to use the express routes
+// const middleware = require('./utils/middleware');
+const mongoose = require('mongoose');
+
+console.log('connecting to', config.URI)
+
+mongoose.connect(config.URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(middleware.requestLogger);
+app.use(postRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+app.listen(config.PORT || 3000, () => {
+    console.log(`Server is running on port: ${config.port}`);
+});
+
+module.exports = app
--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [likes, setLikes] = useState(0);
+  const [comments, setComments] = useState(0);
+  const [post, setPost] = useState([]);
+  const [newPost, setNewPost] = useState('');
+  const [filtered, setFiltered] = useState(post);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/posts') // might change the url
+      .then(response => {
+        console.log('promise fulfilled')
+        setPost(response.data)
+      })
+  }, [])
+
+// Post event changes handling
+const handlePostChange = (event) => {
+  console.log(event.target.value)
+  setNewPost(event.target.value)
+}
+
+// Post basic functionality
+  const addPost = (event) => {
+    event.preventDefault()
+    const postObject = {
+      content: newPost,
+      date: new Date().toISOString(),
+      id: post.length + 1,
+    }
+
+  axios
+  .post('http://localhost:3001/posts', postObject)
+  .then(response => {
+    setPost(post.concat(postObject));
+    console.log(post);
+  })
+  setNewPost('');
+  }
+
+  const searchPosts = (event) => {
+    if(event.target.value !== ""){
+      setFiltered(filtered.filter((post) => {
+        post.toLowerCase().includes(event.target.value.toLowerCase());
+      })
+      )}
+  }
+
+  const deletePosts = (event,id) => { // not sure about the parameter
+    event.preventDefault();
+    let afterDeletion = post;
+    afterDeletion.some((el,i) => {
+      if(el === id){
+        afterDeletion.splice(i,1);
+        return true;
+      }
+    })
+
+    setPost(afterDeletion);
+
+    axios
+    .delete('http://localhost:3001/posts', {params: {id: id} })
+    .then(res => {
+      console.log(res);
+    }) 
+    }
+
+  const DisplayPosts = (props) => {
+    props.input.map(post => {
+      return(
+      <li key={post.id}> {post.content} {post.date}>
+      <span onClick={() => deletePosts(post.id)} />
+      </li>
+      )
+    })
+  }
+
+  const postLikes = () => {
+
+  }
+
+  const postComments = () => {
+
+  }
+
+
+  return(
+    <div>
+    <h1>Posts</h1>
+    <div>
+      <input 
+      type="text" 
+      placeholder="Search..." onChange={searchPosts}/>
     </div>
-  );
+    <DisplayPosts input={filtered} />
+    <div>
+    <form onSubmit={addPost}>
+      <input
+        value={newPost} 
+        onChange={handlePostChange}
+      />
+      <button type="submit">Add Post</button>
+    </form>
+    <DisplayPosts input={post} />
+    </div>
+  </div>
+  )
 }
 
 export default App;
